383c076926caf429ba66d70ad31679e2fd7d9cac
William Horning <github@wgyt.tk>
2021-03-29 09:58:16 -0400

    Update getstring.js

--- localization/getstring.js
+++ localization/getstring.js
@@ -1,6 +1,11 @@
 module.exports = (key,locale) => {
     const path = require("path");
-    const string = require(`./strings.${locale}`)
+    if (path.existsSync(`./strings.${locale}.js`)) { 
+        const string = require(`./strings.${locale}`)
+    } else {
+        console.warn('1host.js isn\'t avalible in the system language, using english.');
+        const string = require(`./strings.en`)
+    }
     // uncomment the code below to test L10N system
     // console.log(string)
     return string[key]
