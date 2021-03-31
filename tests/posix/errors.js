877092e05e2e97642bfe49d6ee649f5f764eed19
GrahamSH <grahamshllk@gmail.com>
2021-03-31 13:23:19 +0000

    lot of docs changes and error changes

--- tests/posix/errors.js
+++ tests/posix/errors.js
@@ -2,7 +2,6 @@ const ejs = require("ejs");
 module.exports = async (req, res, code) => {
   res.statusCode = code;
   var datanum = code;
-  var datadesc = "Hi";
   var dataver = "v2";
   var datadate = new Date(Date.now() * 1000).toLocaleString("en-US", {
     timeZoneName: "short",
@@ -13,9 +12,8 @@ module.exports = async (req, res, code) => {
     await ejs.renderFile("./error.ejs", {
       errornumber: code,
       datanum: datanum,
-      datadesc: datadesc,
       url: `https://${req.host}${req.url}`,
-      host: req.host || "wgytcraft selfhosted",
+      host: req.host || "1host.js selfhosted",
       timestamp: datadate,
       version: dataver,
     })
