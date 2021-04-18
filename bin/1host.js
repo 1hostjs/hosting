45cf1cac6e2ddfc74c789cde664f91a2e991d3b5
William (Wgyt) <williamhorning@wgyt.tk>
2021-04-18 15:25:02 +0000

    oops

--- bin/1host.js
+++ bin/1host.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 if (process.argv[2] === "start") {
-  await import("../src/index.mjs");
+  await import("../src/index.js");
 } else if (process.argv[2] === "config") {
-  await import("./config.mjs");
+  await import("./config.js");
 } else {
-  await import("./help.mjs");
+  await import("./help.js");
 }
